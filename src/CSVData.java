83caf7a6b5d88bd5a11e5d57c594162ff48671ac
ahakem733 <ahakem733@fhs.win.fuhsd.org>
2016-10-12 10:40:25 -0700

    Add method stubs to CSVD data

--- src/CSVData.java
+++ src/CSVData.java
@@ -2,8 +2,22 @@
 //A class to read/write numerical CSV files and allow easy access of data.
 //d
 public class CSVData {
-
+	private double[][] data;
+	private String[] columnNames;
 	
+	public static CSVData readCSVFile (String filename, int numLinesToIgnore, String[] columnNames){
+		return null;
+	}
+	/***
+	 * Returns a new CSVData objec for a file ignoring lines at the top.
+	 * It uses the first row as the column names. All other data is stored as doubles.
+	 * @param filename
+	 * @param numLinesToIgnore
+	 * @return a CSVData object for that file.
+	 */
+	public CSVData readCSVFile (String filename, int numLinesToIgnore){
+		return null;
+	}
 	
 	
 	
