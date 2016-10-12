b79e67671e25a8e6ff9a3d1027852ef81e182f10
ahakem733 <ahakem733@fhs.win.fuhsd.org>
2016-10-12 10:57:16 -0700

    All method stubs

--- src/CSVData.java
+++ src/CSVData.java
@@ -9,7 +9,7 @@ public class CSVData {
 		return null;
 	}
 	/***
-	 * Returns a new CSVData objec for a file ignoring lines at the top.
+	 * Returns a new CSVData object for a file ignoring lines at the top.
 	 * It uses the first row as the column names. All other data is stored as doubles.
 	 * @param filename
 	 * @param numLinesToIgnore
@@ -18,7 +18,30 @@ public class CSVData {
 	public CSVData readCSVFile (String filename, int numLinesToIgnore){
 		return null;
 	}
-	
+	public double[] getRow(int rowIndex){
+		return null;
+	}
+	public double [] getColumn(int colIndex){
+		return null;
+	}
+	public double[][] getRows(int []rowsRequested){
+		return null;
+	}
+	public double [][] getColumns(int []columnsRequested){
+		return null;
+	}
+	public void setVal(int row, int col, double value){
+		return;
+	}
+	public void setCol(int colIndex, double[]newCol){
+		
+	}
+	public void setRow(int rowIndex, double[]newRow){
+		
+	}
+	public String[] getColNames(){
+		return null;
+	}
 	
 	
 	
